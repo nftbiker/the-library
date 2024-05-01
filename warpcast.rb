b0b91beb3fda0f5cce28ac0106f6831f39d3f1e7
NftBiker <me@nftbiker.com>
2024-05-01 11:51:59 +0200

    Api alternative
    To retrieve oldest cast, we use the pinata hub api

--- warpcast.rb
+++ warpcast.rb
@@ -1,18 +1,22 @@
-require 'bundler/inline'
+require "bundler/inline"
 gemfile do
-  source 'https://rubygems.org'
-  gem 'activesupport'
-  gem 'rest-client'
-  gem 'pp'
+  source "https://rubygems.org"
+  gem "activesupport"
+  gem "rest-client"
+  gem "pp"
 end
 
 require "active_support/all"
-require 'json'
+require "json"
 
 JSON_PATH = "./_json"
 MD_PATH = "./_posts"
 AUTH_PATH = "./_authors"
 TIME_FMT = "%Y-%m-%dT%H:%M:%S%z"
+CHANNEL_URL = "https://warpcast.com/~/channel/the-library"
+
+PINATA_URL = "https://hub.pinata.cloud/v1"
+PINATA_ORIGIN_TS = 1609455600 # 2021-01-01 00:00:00 +0100
 
 class Warpcast
   attr_accessor :casts
@@ -30,18 +34,18 @@ class Warpcast
 
   def call(options = {})
     upto = (options[:days] || 2).days.ago.beginning_of_day.to_i
-    puts "Process casts older from #{options[:from] ? Time.at(options[:from]/1000).to_s : 'now'}"
+    puts "Process casts older from #{options[:from] ? Time.at(options[:from] / 1000).to_s : "now"}"
     list = retrieve(options[:from])
     list.each do |item|
-      next if item['pinned']
-      cast = item['cast']
+      next if item["pinned"]
+      cast = item["cast"]
 
       images = get_images_from(cast)
       if images.blank?
         # is it a recast?
-        recasts = (cast['embeds'] || {})['casts'] || []
+        recasts = (cast["embeds"] || {})["casts"] || []
         unless recasts.blank?
-          other = recasts.detect {|e| !e['embeds']['images'].blank?}
+          other = recasts.detect { |e| !e["embeds"]["images"].blank? }
           cast = other if other
         end
         images = get_images_from(cast)
@@ -51,28 +55,28 @@ class Warpcast
 
       # what we memoize
       res = {
-        id: cast['hash'],
-        timestamp: cast['timestamp'],
+        id: cast["hash"],
+        timestamp: cast["timestamp"],
         author: {
-          username: cast['author']['username'],
-          displayname: cast['author']['displayName'],
-          fid: cast['author']['fid'],
-          avatar: cast['author']['pfp']['url'],
-          description: cast['author']['profile']['bio']['text'],
+          username: cast["author"]["username"].to_s.strip,
+          displayname: cast["author"]["displayName"].to_s.strip,
+          fid: cast["author"]["fid"],
+          avatar: cast["author"]["pfp"]["url"],
+          description: cast["author"]["profile"]["bio"]["text"],
         }.stringify_keys,
-        text: cast['text'],
-        images: images
+        text: cast["text"],
+        images: images,
       }
 
       res.stringify_keys!
       save_json(res, options[:force])
       save_markdown(res, options[:force])
-      casts[res['id']] = res
+      casts[res["id"]] = res
     end
 
-    if list.size>=15
-      from = list.last['timestamp']
-      return call(options.merge(from: from)) if from/1000>upto
+    if list.size >= 15
+      from = list.last["timestamp"]
+      return call(options.merge(from: from)) if from / 1000 > upto
     end
     store_authors
   end
@@ -87,79 +91,186 @@ class Warpcast
     store_authors
   end
 
+  # retrieve everything (or only recent casts) from Hub API
+  # https://docs.farcaster.xyz/reference/hubble/httpapi/casts
+  # data are not in the same format as warpcast API but are enough for our need
+  # on pinata, the timestamp is weird (not true epoch)
+  def archives(recent = false)
+    url = File.join(PINATA_URL, "/castsByParent").to_s
+    url += "?url=#{CHANNEL_URL}"
+    url += "&reverse=1&pageSize=#{recent.class == Integer ? recent : 100}" if recent
+    api = RestClient.get(url, { content_type: :json, accept: :json })
+    body = JSON.parse(api.body)
+
+    authors = {}
+    results = []
+
+    body["messages"].each do |data|
+      next if json_exist?(data["hash"])
+
+      cast = data["data"]
+      next if cast["type"] != "MESSAGE_TYPE_CAST_ADD"
+
+      msg = cast["castAddBody"]
+      embeds = msg["embeds"].map { |e| e["url"] }.compact
+      images = embeds.select { |e| e.match(/imagedelivery\.net|imgur\.com|postimg\.cc|supercast\.mypinata|\.(png|jpe?g|gif)/im) }
+      if images.blank? && !embeds.blank?
+        puts "Ignore #{data["hash"]} - Empty image : #{embeds}"
+        next
+      end
+      next if images.blank?
+
+      authors[cast["fid"]] = true
+      # what we memoize
+      res = {
+        id: data["hash"],
+        timestamp: (cast["timestamp"] + PINATA_ORIGIN_TS) * 1000,
+        author: {
+          fid: cast["fid"],
+        }.stringify_keys,
+        text: msg["text"],
+        images: images,
+      }
+      res.stringify_keys!
+      results.push(res)
+    end
+
+    authors = authors_infos(authors.keys)
+    results.each do |item|
+      a = authors[item["author"]["fid"]]
+      if a.blank?
+        puts "ERROR: missing author for #{item["author"]["fid"]}"
+      else
+        item["author"] = a
+        puts "Save cast #{item["id"]}"
+        save_json(item)
+        save_markdown(item)
+      end
+    end
+    true
+  end
+
   private
 
-  def store_authors
+  def authors_infos(author_ids)
+    list = get_authors
+    author_ids.each do |fid|
+      fid = fid.to_i
+      next if list[fid]
+      list[fid] = get_author(fid)
+    end
+    list
+  end
+
+  def get_author(fid)
+    url = File.join(PINATA_URL, "/userDataByFid").to_s
+    url += "?fid=#{fid}"
+    api = RestClient.get(url, { content_type: :json, accept: :json })
+
+    data = JSON.parse(api.body)["messages"].collect do |e|
+      (e["data"] || {})["userDataBody"]
+    end.inject({}) do |hsh, e|
+      k = e["type"].to_s.gsub(/USER_DATA_TYPE_/im, "").downcase
+      hsh[k] = e["value"] unless k.blank?
+      hsh
+    end
+    {
+      username: data["username"].to_s.strip,
+      displayname: data["display"].to_s.strip,
+      fid: fid.to_i,
+      avatar: data["pfp"],
+      description: data["bio"],
+    }.stringify_keys
+  end
+
+  def get_author_key(author, key, type)
+    val = author.detect { |e| e[] }
+  end
+
+  def get_authors
     list = {}
     Dir.glob("#{JSON_PATH}/*.json").each do |f|
       res = JSON.parse(File.read(f))
       author = res["author"]
-      list[author['username']] = author
+      list[author["fid"].to_i] = author
     end
+    list
+  end
 
-    list.values.each do |author|
-      path = File.join(AUTH_PATH, "#{author['username']}.md")
+  def store_authors
+    get_authors.values.each do |author|
+      path = File.join(AUTH_PATH, "#{author["username"]}.md")
       front = []
       front << "---"
-      front << "username: #{author['username']}"
-      front << "displayname: #{author['displayname']}"
-      front << "fid: #{author['fid']}"
-      front << "profile: https://warpcast.com/#{author['username']}"
-      front << "avatar: #{author['avatar']}" if author['avatar']
+      front << "username: #{author["username"]}"
+      front << "displayname: #{author["displayname"]}"
+      front << "fid: #{author["fid"]}"
+      front << "profile: https://warpcast.com/#{author["username"]}"
+      front << "avatar: #{author["avatar"]}" if author["avatar"]
       front << "---"
       front << ""
 
       File.open(path, "wb") do |f|
         f.write(front.join("\n"))
-        f.write("#{author['description'].to_s.strip.gsub("\n", "  \n")}  \n")
+        f.write("#{author["description"].to_s.strip.gsub("\n", "  \n")}  \n")
       end
     end
     true
   end
 
   def get_images_from(cast)
-    ((cast['embeds'] || {})['images'] || []).collect { |e|
-      next if e['type'] != 'image'
-      (e['media'] || {})['staticRaster'] || e['url'] || e['sourceUrl']
+    ((cast["embeds"] || {})["images"] || []).collect { |e|
+      next if e["type"] != "image"
+      (e["media"] || {})["staticRaster"] || e["url"] || e["sourceUrl"]
     }.compact
   end
 
+  def json_exist?(hash)
+    path = File.join(JSON_PATH, "#{hash}.json")
+    File.exist?(path)
+  end
+
   def save_json(entry, force = false)
-    path = File.join(JSON_PATH, "#{entry['id']}.json")
+    path = File.join(JSON_PATH, "#{entry["id"]}.json")
     return path if !force && File.exist?(path)
-    File.open(path, "wb") {|f|f.write(JSON.pretty_generate(entry))}
+    File.open(path, "wb") { |f| f.write(JSON.pretty_generate(entry)) }
     path
   end
 
   def save_markdown(entry, force = false)
-    d = Time.at(entry['timestamp'].to_i/1000).getutc.strftime('%Y-%m-%d-%H%M')
-    id = entry['id'][0,10]
+    d = Time.at(entry["timestamp"].to_i / 1000).getutc.strftime("%Y-%m-%d-%H%M")
+    id = entry["id"][0, 10]
     path = File.join(MD_PATH, "#{d}-#{id}.md")
     return path if !force && File.exist?(path)
-    author = entry['author']
-    img = entry['images'][0]
+    author = entry["author"]
+    img = proper_image(entry["images"][0])
 
     front = []
     front << "---"
-    front << "author: #{author['displayname']}"
-    front << "date: #{Time.at(entry['timestamp']/1000).strftime(TIME_FMT)}"
-    front << "username: #{author['username']}"
-    front << "fid: #{author['fid']}"
-    front << "cast_id: #{entry['id']}"
-    front << "cast: https://warpcast.com/#{author['username']}/#{id}"
+    front << "author: #{author["displayname"]}"
+    front << "date: #{Time.at(entry["timestamp"] / 1000).strftime(TIME_FMT)}"
+    front << "username: #{author["username"]}"
+    front << "fid: #{author["fid"]}"
+    front << "cast_id: #{entry["id"]}"
+    front << "cast: https://warpcast.com/#{author["username"]}/#{id}"
     front << "image: #{img}"
     front << "layout: post"
     front << "---"
     front << ""
 
-    File.open(path, "wb") {|f|
+    File.open(path, "wb") { |f|
       f.write(front.join("\n"))
-      f.write("#{entry['text'].gsub("\n", "  \n")}  \n")
-      entry['images'].each do |i|
-        f.write("\n![](#{i})")
+      f.write("#{entry["text"].gsub("\n", "  \n")}  \n")
+      entry["images"].each do |i|
+        f.write("\n![](#{proper_image(i)})")
       end
     }
     path
+  end
+
+  def proper_image(url)
+    return url unless url.match(/imgur\.com/im)
+    url.gsub(/\.jpg/im, ".jpeg")
   end
 
   def retrieve(from = nil)
@@ -170,10 +281,11 @@ class Warpcast
       viewedCastHashes: "",
       updateState: true,
       latestMainCastTimestamp: from,
-      olderThan:from,
-    }.to_json, {content_type: :json, accept: :json})
-        data = JSON.parse(api.body)
-    (data["result"]||{})["items"]|| []
+      olderThan: from,
+    }.to_json, { content_type: :json, accept: :json })
+
+    data = JSON.parse(api.body)
+    (data["result"] || {})["items"] || []
   end
 end
 
